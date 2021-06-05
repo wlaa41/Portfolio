41294138b0f1e4543c9063cac925200622f0bc17
unknown <wlaa41@gmail.com>
2021-06-06 00:57:56 +0100

    starting the final editing

--- src/components/stackCards/info/info.js
+++ src/components/stackCards/info/info.js
@@ -10,26 +10,38 @@ export default function info(){
             {
                 title: 'Ant Colony Simulation ',
                 subtitle:'python',
-                brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam lorem. Nulla euismod cursus risus ut efficitur. Curabitur enim felis,',
+                brief: 'Finds the short paths by simulating real ant behavior. The uses probabilty to and by repitition it finds short paths without exhausting proccessor resources. I have also add a graph that shows the best paths found',
                 image: ant
             },
             {
                 title: 'ANN Simulationn',
                 subtitle:'python',
-                brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam lorem. Nulla euismod cursus risus ut efficitur. Curabitur enim felis,',
+                brief: 'Artificial Neural Network built from the gruond up without using any third library -Only Numpy- the designed network is apple to classify MINST data with 92 accuracy',
                 image: neural
-            },
-            {
-                title: 'Rasola & Wanger module',
-                subtitle:'python',
-                brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam lorem. Nulla euismod cursus risus ut efficitur. Curabitur enim felis,',
-                image: brain
             },
             {
                 title: "Dijkstra's algorithm",
                 subtitle:'python',
-                brief: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet quam lorem. Nulla euismod cursus risus ut efficitur. Curabitur enim felis,',
+                brief: 'Based on the known algorithem a full python code that run simulationn on 2 dimenssion grid,',
                 image: pathFinding
+            }
+            ,{
+                title: "My Path Finding Algorithm",
+                subtitle:'python',
+                brief: 'A compenation on A star and Ant Colony where it can finds the best path with minium resources. The code graphs a the path found and prints out the passed nodes ',
+                image: ant
+            }
+            ,{
+                title: "Bostom House Price Predection",
+                subtitle:'python',
+                brief: 'runing different macheine learning model on data and get the best result ',
+                image: brain
+            }
+            ,{
+                title: 'Rescorla–Wagner model',
+                subtitle:'python',
+                brief: 'A full simulation of the model ploting a graph on how learning process occure in animal -following Rescorla–Wagner theroy- the siimulation is able demonstrate all different types of learning i.e. Blocking, Negative patterning ...etc. It shows also where the models fall.   ',
+                image: brain
             }
 
 ])}
--- src/layout/EduExp/info/info.js
+++ src/layout/EduExp/info/info.js
@@ -1,7 +1,10 @@
 import ant from './ant-walking-tree.jpg'
 import robot from './robot-arm.jpg'
+import baby from './baby.jpg'
+
 import sprite from './ICON_sprite.svg'
 import React from 'react'
+
 
 
 export const  info = [
@@ -18,7 +21,7 @@ export const  info = [
                         </svg>
             },
             {
-                title: 'Career shiftL: ML, React, and SASS ',
+                title: 'Career shift: ML, AI, & Reactjs ',
                 date: '20 to present',
                 subtitle:'More Than 15 AI. related courses',
                 brief: 'Along side my study, I invested most all my time learning. I have completed more than 14 courses mainly focusing on math i.e. linear algabra, Probability & Statistics, number theory. I am also currently studying ROS2, techniques to build automotive-related algorithms. I compleated and used. Javascript, SASS, React js -FullStack Development-  in few small freelancing project. ',
@@ -28,23 +31,12 @@ export const  info = [
                             </use>
                         </svg>
             },
+            
             {
                 title: 'Smart Cities Company, UAE',
                 date: '2015 - 20',
-                subtitle:'Consultantr',
-                brief: ''
-                ,
-                image: ant,
-                icon:   <svg className="CardEdu_header_icon">
-                            <use href={sprite+"#icon-layers"}>
-                            </use>
-                        </svg>
-            },
-            {
-                title: 'Smart Cities Company, UAE',
-                date: '2015 - 20',
-                subtitle:'Manager & R&D',
-                brief: ' improve existing products Based on industry trends, As working in Home Automation Company I connected our services using noSQL cloud. Develop concepts, products, and solutions by coordinating with sales team. Some the of the concept product was Local Language Voice recognetion system. We used the system that I a have personlly developed with my team in heavly in the company marketing campign as it gives advantge to our home automation solution.  Monitor team metrics and objectives ensuring meeting of goals. Document all phases of research and development.',
+                subtitle:'Manager & R&D Manger',
+                brief: 'Document and place and commision project plan.  Improve existing products Based on industry trends, As working in Home Automation Company I connected our services using noSQL cloud. Teach member of staff on software i.e AutaCAD, Solid Work and Control4 and KNX system. Develop concepts, products, and solutions by coordinating with sales team. Some the of the concept product was Local Language Voice recognetion system. We used the system that I a have personlly developed with my team in heavly in the company marketing campign as it gives advantge to our home automation solution.  Monitor team metrics and objectives ensuring meeting of goals. Document all phases of research and development.',
                 image: ant,
                 icon:   <svg className="CardEdu_header_icon">
                             <use href={sprite+"#icon-layers"}>
@@ -55,13 +47,28 @@ export const  info = [
                 title: 'Int. I. University of Malaysia',
                 date: '2013 - 15',
                 subtitle:'Lab instructor Assitant',
-                brief: 'Before, I decided to take a shift to my career I have been in great position as Electronic and Electrical Engineer. I have manged a project and led a team of 25 engieneers and technichans. I gained a lot of knowledge on how to lead by doing. and also learned that the powers comes when working along side others. Monitor team metrics and objectives ensuring meeting of goals. Document all phases of research and development',
+                brief: `Teach as a member of a teaching team within an established programme of study involving PCB circuit design and microcontroller programming. Develop own teaching materials, methods and approaches taking into account
+                established or agreed practices where necessary.Encourage pupils to interact and experment. Set challenging and demanding expectations and promote self-esteem and
+                independence.`
+                ,
                 image: robot,
                 icon:   <svg className="CardEdu_header_icon">
                             <use href={sprite+"#icon-blackboard"}>
                             </use>
                         </svg>
             },
+            // {
+            //     title: 'Born in 1986',
+            //     date: '2013 - 15',
+            //     subtitle:'Eye opener',
+            //     brief: `My love to my mother, late father. Also my supportiv wife and great family around me.`
+            //     ,
+            //     image: baby,
+            //     icon:   <svg className="CardEdu_header_icon">
+            //                 <use href={sprite+"#icon-blackboard"}>
+            //                 </use>
+            //             </svg>
+            // }
 ]
 
 
