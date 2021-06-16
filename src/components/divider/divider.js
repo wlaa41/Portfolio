5ac0397509efbaca3af96b28368e0f355b057c0d
wlaa41 <wlaa41@GMAIL.COM>
2021-06-16 01:55:56 +0100

    deleting the style in divider as i suspect it cause extra white space on the end of the page as it has require method

--- src/components/divider/divider.js
+++ src/components/divider/divider.js
@@ -5,10 +5,6 @@ import './divider.scss'
 
 export default function Divider({style}){
     // console.log({style})
-    return <div 
-        style={{...style , color:"#333333",
-         backgroundImage: `url(require("./brickwallSmall2.png"))` }}
-          className='divider'>
-
+    return <div className='divider'>
           </div>
 }
