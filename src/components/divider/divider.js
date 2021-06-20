bf4e63f9c4d259950e242b8e989ee5120c9795d5
wlaa41 <wlaa41@GMAIL.COM>
2021-06-20 12:16:04 +0100

    add svg clip path

--- src/components/divider/divider.js
+++ src/components/divider/divider.js
@@ -7,6 +7,7 @@ import './divider.scss'
 export default function Divider({style}){
     // console.log({style})
     return<div className='divider'>
+            <svg width="0" height="0"><defs><clipPath id="myCurve" clipPathUnits="objectBoundingBox"><path d="M1,1H0V0A.06.06,0,0,1,.06,0C.17.23.35.85.5.85S.83.31.94,0C1,0,1,0,1,0Z" /> </clipPath> </defs>    </svg>
         <Arrow></Arrow>
     </div>
 }
